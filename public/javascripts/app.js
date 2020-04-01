1f241bc59f74963ae6fd1b4fe89d4a3b6694036c
petenet001 <petenet001@gmail.com>
2020-04-01 09:40:17 +0100

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    // Month Day, Year Hour:Minute:Second, id-of-element-container
+      countDownToTime("Apr 31, 2020 "+ new Date().getTime(), 'countdown1'); // ****** Change this line!
+    }
+    function countDownToTime(countTo, id) {
+      countTo = new Date(countTo).getTime();
+      var now = new Date(),
+          countTo = new Date(countTo),
+          timeDifference = (countTo - now);
+          
+      var secondsInADay = 60 * 60 * 1000 * 24,
+          secondsInAHour = 60 * 60 * 1000;
+    
+      days = Math.floor(timeDifference / (secondsInADay) * 1);
+      hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+      mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+      secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+    
+      var idEl = document.getElementById(id);
+      idEl.getElementsByClassName('days')[0].innerHTML = days;
+      idEl.getElementsByClassName('hours')[0].innerHTML = hours;
+      idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
+      idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+    
+      clearTimeout(countDownToTime.interval);
+      countDownToTime.interval = setTimeout(function(){ countDownToTime(countTo, id); },1000);
+
+      var aud = document.getElementById("myAudio");
+      aud.volume = 0.5; // default 1 means 100%
+    }
